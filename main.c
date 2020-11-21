25157fa20d3a9789432553417721c59c443565c2
vasanth <smkrvasu@gmail.com>
2020-11-21 13:15:46 +0800

    Given a personalized hello message.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(int argc, char **argv)
 {
-	printf("Hello world!\n");
+	char name[255];
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	name[strlen(name)-1] = '\0'; /* remove the new line at the end */
+	printf("Hello %s!\n", name);
 	return 0;
 }
